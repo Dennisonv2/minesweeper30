8d2714551f47583ff4b050c7b9a6541ea2e7884d
Denis_Karimov <karimov.lololowka2017@ya.ru>
2024-02-23 22:52:55 +0500

    2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
-// The main class that contains the main method, the entry point of the program.
 public class Main {
-    // The main method is the starting point of the program. It creates an instance of the Minesweeper class.
     public static void main(String[] args) {
-        // The following line creates an instance of the Minesweeper class.
         new Minesweeper();
     }
 }
